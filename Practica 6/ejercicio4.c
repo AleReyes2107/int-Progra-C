f969ca7a01e113b311c9ffe1124e00b445514c52
Ale :) <125687312+AleReyes2107@users.noreply.github.com>
2023-05-01 11:43:35 -0600

    Update ejercicio4.c

--- Practica 6/ejercicio4.c
+++ Practica 6/ejercicio4.c
@@ -20,25 +20,23 @@ int main() {
     char Palarga[TAM];
     int Locorta = TAM;
     int Lolarga = 0;
-    char *aux;
+    int n = 0, i = 0;
 
     printf("Ingrese una cadena de texto: ");
     fgets(cadena, TAM, stdin);
 
     // Buscar la palabra más corta y la palabra más larga
-    aux = strtok(cadena, " \n");  // Dividir la cadena en palabras usando espacios y saltos de línea como delimitadores
-    while (aux != NULL) {
-        int LoPa = strlen(aux);
-        if (LoPa < Locorta) {
-            strncpy(Pacorta, aux, TAM);  // Copiar la palabra a la variable de la palabra más corta
-            Locorta = LoPa;  // Actualizar la longitud de la palabra más corta
-        }
-        if (LoPa > Lolarga) {
-            strncpy(Palarga, aux, TAM);  // Copiar la palabra a la variable de la palabra más larga
-            Lolarga = LoPa;  // Actualizar la longitud de la palabra más larga
-        }
-        aux = strtok(NULL, " \n");  // Obtener la siguiente palabra
+   for (int n = 0, i = 0; sscanf(cadena + n, " %s%n", Pacorta, &i) != EOF; n += i) {
+    int LoPa = strlen(Pacorta);
+    if (LoPa < Locorta) {
+        sprintf(Pacorta, "%s", Pacorta);  // Copiar la palabra a la variable de la palabra más corta
+        Locorta = LoPa;  // Actualizar la longitud de la palabra más corta
     }
+    if (LoPa > Lolarga) {
+        sprintf(Palarga, "%s", Pacorta);  // Copiar la palabra a la variable de la palabra más larga
+        Lolarga = LoPa;  // Actualizar la longitud de la palabra más larga
+    }
+}
 
     // Imprimir las palabras más corta y más larga
     printf("La palabra mas corta es: %s\n", Pacorta);
